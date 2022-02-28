5502998d18a19206dd88e9dc1e7261dba177251c
Harsh R <harsh97.007@gmail.com>
2022-02-28 20:16:00 +0530

    plot3 Rprog

---
+++ plot3.R
@@ -0,0 +1,16 @@
+setwd("./Coursera")
+txt_data<- read.csv("household_power_consumption.txt",header=TRUE, stringsAsFactors=FALSE, sep=';',na.strings="?",nrows=2075259)
+sub_data<- subset(txt_data, txt_data$Date=="1/2/2007" | txt_data$Date=="2/2/2007")
+
+dt <- strptime(paste(sub_data$Date, sub_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+gap<-as.numeric(sub_data$Global_active_power)
+smt1<-as.numeric(sub_data$Sub_metering_1)
+smt2<-as.numeric(sub_data$Sub_metering_2)
+smt3<-as.numeric(sub_data$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(dt, smt1, type="l", xlab="", ylab="Energy Submetering")
+lines(dt, smt2, type="l", col="red")
+lines(dt, smt3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, col=c("black", "red", "blue"))
+dev.off()
